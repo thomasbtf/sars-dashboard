ecba69ea71d254f10fdffc7d6173d3a8d9a7f1a5
Thomas Battenfeld <thomas.battenfeld@gmail.com>
2021-12-11 19:05:45 +0100

    add skeleton

---
+++ sars_dashboard/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from sars_dashboard.users.models import User
+from sars_dashboard.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
