9a5d74acc57912e2c597f6583c38926a8d1e6cc5
Thomas Battenfeld <thomas.battenfeld@gmail.com>
2021-12-16 23:25:23 +0100

    cleanup

--- reports/admin.py
+++ reports/admin.py
@@ -1,17 +1,7 @@
 from django.contrib import admin
 
-from .models import (
-    ModifedReportIndex,
-    ReportFiles,
-    ReportIndex,
-    ReportRawData,
-    ReportThumbnails,
-    ZippedReport,
-)
+from .models import ReportFiles, ReportIndex, ZippedReport
 
 admin.site.register(ZippedReport)
 admin.site.register(ReportFiles)
-admin.site.register(ModifedReportIndex)
 admin.site.register(ReportIndex)
-admin.site.register(ReportRawData)
-admin.site.register(ReportThumbnails)
--- reports/models.py
+++ reports/models.py
@@ -3,35 +3,7 @@ import os
 from django.db import models
 
 
-def get_report_raw_path(instance, filename):
-    path = "report-files/{report_name}/data/raw".format(
-        report_name=instance.zipped_report.zip_file.name.removesuffix(".zip")
-    )
-    return os.path.join(path, filename)
-
-
-def get_report_thumpnail_path(instance, filename):
-    path = "report-files/{report_name}/data/thumbnails".format(
-        report_name=instance.zipped_report.zip_file.name.removesuffix(".zip")
-    )
-    return os.path.join(path, filename)
-
-
-def get_report_index_path(instance, filename):
-    path = "report-files/{report_name}/".format(
-        report_name=instance.zipped_report.zip_file.name.removesuffix(".zip")
-    )
-    return os.path.join(path, filename)
-
-
-def get_report_mod_index_path(instance, filename):
-    path = "report-files/{report_name}/".format(
-        report_name=instance.zipped_report.zip_file.name.removesuffix(".zip")
-    )
-    return os.path.join(path, filename)
-
-
-def get_report_file_path(instance, filename):
+def get_report_path(instance, filename):
     path = "report-files/{report_name}/".format(
         report_name=instance.zipped_report.zip_file.name.removesuffix(".zip")
     )
@@ -46,30 +18,12 @@ class ZippedReport(models.Model):
 
 class ReportFiles(models.Model):
     zipped_report = models.ForeignKey(ZippedReport, on_delete=models.CASCADE)
-    file = models.FileField(upload_to=get_report_file_path, max_length=255)
+    file = models.FileField(upload_to=get_report_path, max_length=255)
     original_name = models.CharField(max_length=255, blank=True)
     is_index = models.BooleanField(default=False)
 
 
-class ReportRawData(models.Model):
-    zipped_report = models.ForeignKey(ZippedReport, on_delete=models.CASCADE)
-    file = models.FileField(upload_to=get_report_raw_path, max_length=255)
-    original_name = models.CharField(max_length=255, blank=True)
-
-
-class ReportThumbnails(models.Model):
-    zipped_report = models.ForeignKey(ZippedReport, on_delete=models.CASCADE)
-    file = models.FileField(upload_to=get_report_thumpnail_path, max_length=255)
-    original_name = models.CharField(max_length=255, blank=True)
-
-
 class ReportIndex(models.Model):
     zipped_report = models.ForeignKey(ZippedReport, on_delete=models.CASCADE)
-    file = models.FileField(upload_to=get_report_index_path, max_length=255)
-    original_name = models.CharField(max_length=255, blank=True)
-
-
-class ModifedReportIndex(models.Model):
-    zipped_report = models.ForeignKey(ZippedReport, on_delete=models.CASCADE)
-    file = models.FileField(upload_to=get_report_mod_index_path, max_length=255)
+    file = models.FileField(upload_to=get_report_path, max_length=255)
     original_name = models.CharField(max_length=255, blank=True)
