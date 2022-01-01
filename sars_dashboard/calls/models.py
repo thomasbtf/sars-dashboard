f0dc28c53dde348bd6908a00c6bc7ff3c81ec49e
Thomas Battenfeld <46334240+thomasbtf@users.noreply.github.com>
2022-01-01 21:28:32 +0100

    Update sars_dashboard/calls/models.py
    Co-authored-by: code-review-doctor[bot] <72320148+code-review-doctor[bot]@users.noreply.github.com>

--- sars_dashboard/calls/models.py
+++ sars_dashboard/calls/models.py
@@ -7,7 +7,7 @@ from sars_dashboard.samples.models import Sample
 class MinMaxFloat(models.FloatField):
     def __init__(self, min_value=None, max_value=None, *args, **kwargs):
         self.min_value, self.max_value = min_value, max_value
-        super(MinMaxFloat, self).__init__(*args, **kwargs)
+        super().__init__(*args, **kwargs)
 
     def formfield(self, **kwargs):
         defaults = {"min_value": self.min_value, "max_value": self.max_value}
