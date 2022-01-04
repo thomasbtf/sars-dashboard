73f1a9f90537337d58a4c5bbe1b3468dcdf35637
Thomas Battenfeld <thomas.battenfeld@gmail.com>
2022-01-04 13:40:40 +0100

    add meta change

--- sars_dashboard/samples/models.py
+++ sars_dashboard/samples/models.py
@@ -18,8 +18,9 @@ class Sample(models.Model):
     date_modified = models.DateTimeField(auto_now=True)
     creation_date = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        ordering = ["-date"]
+    
     def __str__(self):
         return self.name
 
-    class Meta:
-        ordering = ["-date"]
