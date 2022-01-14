cb7d0ea9e7cad577411f2338302eb414d1b90411
Thomas Battenfeld <thomas.battenfeld@gmail.com>
2022-01-14 15:43:05 +0000

    fix(production): use local dns zone

--- config/settings/production.py
+++ config/settings/production.py
@@ -14,7 +14,7 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["tbd.ikim.uk-essen.de"])
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["apps.ikim.uk-essen.de"])
 
 # DATABASES
 # ------------------------------------------------------------------------------
